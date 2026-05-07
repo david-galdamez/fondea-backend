d2f6fb3036baa1f71af0fd90533859110440dfc2
David Galdamez <david.galdamez2014@gmail.com>
2026-05-07 12:40:04 -0600

    feat: initialized project with all dependencies

---
+++ src/main/java/com/project/fondea/FondeaApplication.java
@@ -0,0 +1,13 @@
+package com.project.fondea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FondeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FondeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/fondea/FondeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fondea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FondeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
